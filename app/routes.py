5244173a3bbae13fd76419841b2e80cd73f9e4a0
unknown <lukasdpikesevg@gmail.com>
2025-10-14 01:25:34 -0500

    Conexión de páginas, mejoras de navegación

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+
 from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
@@ -5,3 +6,14 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def home():
     return render_template('index.html')
+
+
+# Ruta para Login
+@main.route('/login')
+def login():
+    return render_template('Login.html')
+
+# Ruta para Registro
+@main.route('/registrarse')
+def registrarse():
+    return render_template('Registrarse.html')
